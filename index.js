e445b2e8fd4af9f9a8e1a4d2cbbb5d68d886279f
yuda ferry <juncyboy@gmail.com>
2014-08-11 16:17:10 +0700

    in progress

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ try {
 		init.commander
 		.version(init.package.version)
 		.option("-t, --testpath [testpath]","folder path place for test file",base+sp+"test")
+		.option("-l, --list","list file tested")
+		.option("-c, --choose [number or string]","test specified test file, not all file tested")
 		.parse(process.argv);
 	}
 
@@ -60,6 +62,14 @@ try {
 			var checkDir = init.fs.existsSync(params.testpath);
 			init.assert(checkDir,"folder "+params.testpath+" doesn't exists");
 			init.commander.testpath = params.testpath;
+		}
+
+		if(params.list) {
+			init.commander.list = params.list;
+		}
+
+		if(params.choose) {
+			init.commander.choose = params.choose;
 		}
 	}
 
@@ -82,48 +92,66 @@ try {
 		);
 	}
 
+	var processReq = function(params,cb) {
+		var option = {
+			url 		: params.request.url,
+			headers 	: params.request.headers || {},
+			method 		: params.request.method=="post"?"POST":(params.request.method=="get"?"GET":params.request.method)
+		}
+
+		if(option.method=="POST") {
+			if(params.request.body && !params.request.headers) {
+				option.headers["Content-Type"] = "text/plain";
+			}else if(params.request.body && params.request.headers) {
+				if(!params.request.headers["Content-Type"])
+					option.headers["Content-Type"] = "text/plain";
+			}
+		}
+
+		init.request(option,function(err,res,body) {
+			try {
+				if(params.response) {
+					if(params.response.statusCode) {
+						init.assert(res.statusCode);
+						init.assert.equal(res.statusCode,params.response.statusCode,"statusCode not :"+params.response.statusCode);
+					}
+				}
+
+				if(params.callback) {
+					params.callback.apply(init,[err,res,body]);
+				}
+
+				cb();
+			}catch(e) {
+				cb(e.message);
+			}
+		})
+	}
+
 	var eachWaterfall = function(withArg,params) {
+
 		if(withArg) {
 			return function(arg,cb) {
-
+				processReq(params,cb);
 			}
 		}else {
 			return function(cb) {
-				var option = {
-					url 		: params.request.url,
-					headers 	: params.request.headers || {},
-					method 		: params.request.method=="post"?"POST":(params.request.method=="get"?"GET":params.request.method)
-				}
-
-				if(option.method=="POST") {
-					if(params.request.body && !params.request.headers) {
-						option.headers["Content-Type"] = "text/plain";
-					}else if(params.request.body && params.request.headers) {
-						if(!params.request.headers["Content-Type"])
-							option.headers["Content-Type"] = "text/plain";
-					}
-				}
-
-				init.request(option,function(err,res,body) {
-					try {
-						if(params.response) {
-							if(params.response.statusCode) {
-								init.assert(res.statusCode);
-								init.assert.equal(res.statusCode,params.response.statusCode,"statusCode not :"+params.response.statusCode);
-							}
-						}
-
-						if(params.callback) {
-							params.callback.apply(init,[err,res,body]);
-						}
-
-						cb();
-					}catch(e) {
-						cb(e.message);
-					}
-				})
+				processReq(params,cb);
 			}
 		}
+	}
+
+	var createList = function(testList) {
+		init.assert(testList,"no file to test");
+		init.assert((typeof testList)=="object","testList not an object");
+
+		log("\n");
+		testList.forEach(function(values,keys) {
+			log(keys+" = "+values);
+		})
+		log("\n");
+		log("you can run test with argument : -c 0");
+		log("\n");
 	}
 
 	var exportsThis = function(params) {
@@ -143,53 +171,77 @@ try {
 
 			var count 		= 0;
 			var allKeys 	= Object.keys(init.testList);
-			init.async
-			.until(
-				function() { return count==allKeys.length; },
-				function(cb) {
-					var currentFile 	= allKeys[count];
-					if(currentFile) {
-						var listBatch = {}
-							listBatch[currentFile] = {
-								topic : function() {
-									log("\nrun testfile : "+currentFile);
-									waterfallReq(init.testList[currentFile],this.callback)
-								},
-								"result" : function(err,res) {
-									init.assert(!err,err);
-									count++;
-									cb();
-								}
-							}
-						init.vows
-						.describe("run file : "+currentFile)
-						.addBatch(listBatch)
-						.run();
-					}else {
-						count++;
-						cb();
-					}
-				},
-				function(err) {
-					var endProcess = +(new Date());
-					if(err) {
-						log(err);
-					}else {
-						log("done")
-					}
+			var choose 		= false;
 
-					var range = endProcess - startProcess;
-					if(range>1000) {
-						var second 	= math.round(range/1000);
-						var mili 	= range-(second*1000);
-						range 		= second+" s "+mili+" ms";
-					}else {
-						range 		= range+" ms";
-					}
-					log("\n");
-					log("Total run time :"+range);
+			if(init.commander.choose) {
+				var getChoose = parseInt(init.commander.choose);
+				if(getChoose=="NaN") {
+					count = allKeys.indexOf(init.commander.choose);
+				}else {
+					count 	= getChoose;
 				}
-			)
+				init.assert(count<allKeys.length, "choose number more than total file, or file doesn't exists");
+				choose = count+1;
+			}
+
+			if(init.commander.list) { // if tester ask about list of test file or want to choose one
+				createList(allKeys);
+			}else {
+				init.async
+				.until(
+					function() {
+						if(choose) {
+							return count==choose;
+						}else {
+							return count==allKeys.length;
+						}
+					},
+					function(cb) {
+						var currentFile 	= allKeys[count];
+						if(currentFile) {
+							var listBatch = {}
+								listBatch[currentFile] = {
+									topic : function() {
+										log("\nrun testfile : "+currentFile);
+										waterfallReq(init.testList[currentFile],this.callback)
+									},
+									"result" : function(err,res) {
+										init.assert(!err,err);
+										count++;
+										cb();
+									}
+								}
+
+							init.vows
+							.describe("run file : "+currentFile)
+							.addBatch(listBatch)
+							.run();
+						}else {
+							count++;
+							cb();
+						}
+					},
+					function(err) {
+						var endProcess = +(new Date());
+						log("\n");
+						if(err) {
+							log(err);
+						}else {
+							log("done");
+						}
+
+						var range = endProcess - startProcess;
+						if(range>1000) {
+							var second 	= Math.floor(range/1000);
+							var mili 	= range-(second*1000);
+							range 		= second+" s "+mili+" ms";
+						}else {
+							range 		= range+" ms";
+						}
+						log("Total run time :"+range);
+					}
+				)
+			}
 		}catch(e) {
 			log(e.message);
 			process.exit();
